14db222fd4bae5b00d0eadd40bf4e65ce40460e8
hashem78 <hashem.olayano@gmail.com>
2026-03-24 23:39:25 +0300

    feat: add structlog for structured logging

--- src/spud/__main__.py
+++ src/spud/__main__.py
@@ -2,9 +2,13 @@
 #
 # SPDX-License-Identifier: MIT
 
+import structlog
+
+logger = structlog.get_logger(__name__)
+
 
 def main() -> None:
-    print("spud")
+    logger.info("Hello World!")
 
 
 if __name__ == "__main__":
