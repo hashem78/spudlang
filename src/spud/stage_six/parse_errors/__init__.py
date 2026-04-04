ec1d2f75581c6e553018b44c216ee4ff07547709
hashem78 <hashem.olayano@gmail.com>
2026-04-04 23:03:36 +0300

    refactor(stage_six/parse_errors): add __all__ exports

--- src/spud/stage_six/parse_errors/__init__.py
+++ src/spud/stage_six/parse_errors/__init__.py
@@ -1,1 +1,17 @@
+from spud.stage_six.parse_errors.parse_context import ParseContext, ctx
+from spud.stage_six.parse_errors.parse_context_kind import ParseContextKind
+from spud.stage_six.parse_errors.parse_error import ParseError, with_context
+from spud.stage_six.parse_errors.parse_error_kind import ParseErrorKind
+from spud.stage_six.parse_errors.unexpected_end_error import UnexpectedEndError
+from spud.stage_six.parse_errors.unexpected_token_error import UnexpectedTokenError
 
+__all__ = [
+    "ParseContext",
+    "ParseContextKind",
+    "ParseError",
+    "ParseErrorKind",
+    "UnexpectedEndError",
+    "UnexpectedTokenError",
+    "ctx",
+    "with_context",
+]
