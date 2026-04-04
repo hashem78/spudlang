7ce595354daf4ecb41ea8ead49718790782d91f6
hashem78 <hashem.olayano@gmail.com>
2026-04-04 23:09:17 +0300

    refactor(spud_check): add __all__ exports

---
+++ src/spud_check/__init__.py
@@ -0,0 +1,14 @@
+from spud_check.builtin_types import BUILTIN_TYPES
+from spud_check.container import CheckContainer
+from spud_check.operator_types import BINARY_OP_TYPES, UNARY_OP_TYPES
+from spud_check.type_check_result import TypeCheckResult
+from spud_check.type_checker import TypeChecker
+
+__all__ = [
+    "BINARY_OP_TYPES",
+    "BUILTIN_TYPES",
+    "CheckContainer",
+    "TypeCheckResult",
+    "TypeChecker",
+    "UNARY_OP_TYPES",
+]
