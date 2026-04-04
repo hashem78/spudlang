8a7f7345315020fb65203c2a9749a9f29b5471e1
hashem78 <hashem.olayano@gmail.com>
2026-04-04 23:05:20 +0300

    refactor(core/pipeline): add __all__ exports

--- src/spud/core/pipeline/__init__.py
+++ src/spud/core/pipeline/__init__.py
@@ -1,12 +1,27 @@
-from spud.core.pipeline.parse_step import ParseStep as ParseStep
-from spud.core.pipeline.parsed_program import ParsedProgram as ParsedProgram
-from spud.core.pipeline.pipeline import Pipeline as Pipeline
-from spud.core.pipeline.pipeline_stage import PipelineStage as PipelineStage
-from spud.core.pipeline.pipeline_step import PipelineStep as PipelineStep
-from spud.core.pipeline.resolve_step import ResolveStep as ResolveStep
-from spud.core.pipeline.resolved_program import ResolvedProgram as ResolvedProgram
-from spud.core.pipeline.stage_five_step import StageFiveStep as StageFiveStep
-from spud.core.pipeline.stage_four_step import StageFourStep as StageFourStep
-from spud.core.pipeline.stage_one_step import StageOneStep as StageOneStep
-from spud.core.pipeline.stage_three_step import StageThreeStep as StageThreeStep
-from spud.core.pipeline.stage_two_step import StageTwoStep as StageTwoStep
+from spud.core.pipeline.parse_step import ParseStep
+from spud.core.pipeline.parsed_program import ParsedProgram
+from spud.core.pipeline.pipeline import Pipeline
+from spud.core.pipeline.pipeline_stage import PipelineStage
+from spud.core.pipeline.pipeline_step import PipelineStep
+from spud.core.pipeline.resolve_step import ResolveStep
+from spud.core.pipeline.resolved_program import ResolvedProgram
+from spud.core.pipeline.stage_five_step import StageFiveStep
+from spud.core.pipeline.stage_four_step import StageFourStep
+from spud.core.pipeline.stage_one_step import StageOneStep
+from spud.core.pipeline.stage_three_step import StageThreeStep
+from spud.core.pipeline.stage_two_step import StageTwoStep
+
+__all__ = [
+    "ParseStep",
+    "ParsedProgram",
+    "Pipeline",
+    "PipelineStage",
+    "PipelineStep",
+    "ResolveStep",
+    "ResolvedProgram",
+    "StageFiveStep",
+    "StageFourStep",
+    "StageOneStep",
+    "StageThreeStep",
+    "StageTwoStep",
+]
